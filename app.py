0a9ed31ec1b8c4c6cc3c9e9d1cc785f71f384fcb
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-09-17 11:34:36 +0200

    app.py: LookupByWikidataQid: get(): Support the Enum and return the values. Return 404 if no match. enums.py: New file with enum Return lookup_wikicitations_qid.py: LookupWikicitationsQid: lookup_via_query_service(): Return enums instead of strings. test_lookup_wikicitations_qid.py: 4 new tests.

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ a Wikipedia article and the corresponding Wikicitations item
 https://rapidapi.com/blog/how-to-build-an-api-in-python/
 """
 import logging
+from typing import Union
 
 from flask import Flask
 from flask_restful import Api, Resource
 
 import config
+from models.enums import Return
 from models.lookup_wikicitations_qid import LookupWikicitationsQid
 
 logging.basicConfig(level=config.loglevel)
@@ -21,9 +23,15 @@ class LookupByWikidataQid(Resource):
     def get(self, qid=""):
         if qid:
             lwq = LookupWikicitationsQid()
-            return lwq.lookup_via_query_service(wdqid=qid), 200
+            result: Union[Return, str] = lwq.lookup_via_query_service(wdqid=qid)
+            if isinstance(result, str):
+                return result, 200
+            elif result == Return.NO_MATCH:
+                return result.value, 404
+            else:
+                return result.value, 400
         else:
-            return "No Wikidata QID was given", 400
+            return Return.NO_QID.value, 400
 
 
 # api.add_resource(LookupByLabel, "/lookup-by-label/<str:label>")
